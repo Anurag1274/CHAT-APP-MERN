b499cf7fad872d5135144a2322d60cf654735e8f
Aurag1274 <anuragajbhiye89@gmail.com>
2024-07-29 15:17:14 +0530

    Production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import path from "path";
-import { fileURLToPath } from "url";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,18 +7,14 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-
 import connectToMongoDB from "./db/connectMongoose.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
-
-console.log(__dirname);
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -30,14 +25,9 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-console.log(path.join(__dirname, "/frontend/dist"));
-
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-
-console.log(path.join(__dirname, "/frontend/dist/index.html"))
-
 
 server.listen(PORT, () => {
 	connectToMongoDB();
