916a86bec19f915bf54711b075453e50b8f40074
Aurag1274 <anuragajbhiye89@gmail.com>
2024-10-26 18:47:24 +0530

    Update build and server configuration for Vercel deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-mern-7fvm.onrender.com", {
+			const socket = io("https://chat-app-mern-1-ku1c.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
